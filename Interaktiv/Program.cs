8d29df142d832c74fd63ac8a6a7410f42999d19f
Sador Tilahun <sador.tilahun@elev.ga.ntig.se>
2024-08-30 14:51:03 +0200

    Interaktiv

--- Interaktiv/Program.cs
+++ Interaktiv/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.WriteLine("Användarnamn: ");
+string name = Console.ReadLine();
+
+if (name == "Sador")
+{
+ Console.WriteLine($"Välkommen! {name}");
+}
+else
+{
+ Console.WriteLine($"Nämen fan. Välkommen i guess {name}");
+}
+
+Console.ReadLine();
